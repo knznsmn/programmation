3c50ac5f6db2e9e3fa60a4c28af0b03e7af885f1
Julius Cinco Cesar <knznsmn@icloud.com>
2024-08-15 19:04:17 +0400

    Start chapter 2

---
+++ js/exercises/jsfbtp/chapitre-02/02_concatenation.js
@@ -0,0 +1,5 @@
+const name = 'Julius',
+      age  = 36,
+      canCode = true;
+
+console.log('Salut, my name is ' + name + ', I am ' + age + ' years old and I can code JavaScript: ' + canCode + '.');
