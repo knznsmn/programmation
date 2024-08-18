f5ce1ffe9a914bab53388a43ff05cbe18efc2a0e
Julius Cinco Cesar <157534723+knznsmn@users.noreply.github.com>
2024-08-18 23:18:12 +0400

    Add 02_array-manipulation.js index.html

---
+++ js/jsfbtp/chapitre-03/02_array-manipulation.js
@@ -0,0 +1,8 @@
+const shoppingList = [];
+
+shoppingList.splice(0, 0, 'Bananas', 'Carrots', 'Lettuce', 'Eggs', 'Milk', 'Juice', 'Pop', 'Juice', 'Pop');
+/*
+shoppingList.push('Bananas', 'Carrots', 'Lettuce', 'Eggs', 'Milkie', 'Juice', 'Pop', 'Juice', 'Pop');
+*/
+
+console.log(shoppingList);
