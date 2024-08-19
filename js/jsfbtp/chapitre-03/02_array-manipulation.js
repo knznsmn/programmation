046185040286998d123b6f107dabf0d416abf0dd
Julius Cinco Cesar <157534723+knznsmn@users.noreply.github.com>
2024-08-19 23:41:15 +0400

    Add 02_array-manipulation.js

--- js/jsfbtp/chapitre-03/02_array-manipulation.js
+++ js/jsfbtp/chapitre-03/02_array-manipulation.js
@@ -1,8 +1,15 @@
 const shoppingList = [];
 
-shoppingList.splice(0, 0, 'Bananas', 'Carrots', 'Lettuce', 'Eggs', 'Milk', 'Juice', 'Pop', 'Juice', 'Pop');
+shoppingList.splice(0, 0, 'Milk', 'Bread', 'Apple');
+console.log(shoppingList);
 /*
 shoppingList.push('Bananas', 'Carrots', 'Lettuce', 'Eggs', 'Milkie', 'Juice', 'Pop', 'Juice', 'Pop');
 */
 
+shoppingList.splice(1, 1, 'Bananas', 'Eggs')
 console.log(shoppingList);
+
+shoppingList.pop();
+console.log('pop()', shoppingList);
+
+
