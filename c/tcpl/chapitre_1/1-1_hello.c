7c988d5e291f5d29496ecfef2e053cd5e3f4b3fc
Julius Cinco Cesar <knznsmn@icloud.com>
2025-02-24 12:15:41 +0400

    Add tcpl

---
+++ c/tcpl/chapitre_1/1-1_hello.c
@@ -0,0 +1,10 @@
+// Exercise 1-1. Run the "hello, world" program on your system. Experiment
+// with leaving out parts of the program, to see what errors messages you get.
+
+
+#include <stdio.h>
+
+int main() {
+
+  printf("Hello, world!\n");
+}
